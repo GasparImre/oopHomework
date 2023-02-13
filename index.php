ac5a4e85ecd1e16bd2486faa03cea546080e167e
Imre Gáspár <ifj.gaspar.imre@gmail.com>
2023-02-13 15:36:47 +0100

    Create a small "form"
    the user can interract with it what type of pancake would he/she chose. (It works only on command line)

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
 include_once "Pancake.php";
+include_once "Jam.php";
+include_once "Cocoa.php";
+include_once "Nutella.php";
+//
+//$cocoaPancake = new Pancake("Cocoa");
+//$nutellaPancake = new Pancake("Nutella");
+//$jamPancake = new Pancake("Jam");
 
-$cocoaPancake = new Pancake("Cocoa");
-$nutellaPancake = new Pancake("Nutella");
-$jamPancake = new Pancake("Jam");
+echo " We have Nutella (type: nutella), Jam (type: jam) and Cocoa ( type: cocoa) filled pancackes <br>";
+$chosenType = readline('Enter what kind of pancacke you want:');
+
+$pancake = match ($chosenType) {
+    "nutella" => new Nutella("medium"),
+    "cocoa" => new Cocoa("Small"),
+    "jam" => new Jam("Large"),
+    default => new Cocoa("Small"),
+};
